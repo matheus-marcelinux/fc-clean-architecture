78ad69bab10d58e033d4e7bfc744ee407e7f85cb
matheus-marcelinux <matheusmarcelino1998@hotmail.com>
2024-02-14 10:43:18 -0300

    notification pattert customer e product

--- src/domain/customer/entity/customer.spec.ts
+++ src/domain/customer/entity/customer.spec.ts
@@ -6,13 +6,19 @@ describe("Customer unit tests", () => {
     it("should throw error when id is empty", () => {
         expect(() => {
             let  customer = new Customer("", "Jhon");
-        }).toThrow("ID is required");
+        }).toThrow("customer: Id is required");
     });
 
     it("should throw error when id is empty", () => {
         expect(() => {
             let  customer = new Customer("123", "");
-        }).toThrow("Name is required");
+        }).toThrow("customer: Name is required");
+    });
+
+    it("should throw error when name and id is empty", () => {
+        expect(() => {
+            let  customer = new Customer("", "");
+        }).toThrow("customer: Name is required,customer: Id is required");
     });
 
     it("should change name", () => {
--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -5,13 +5,19 @@ describe("Product unit tests", () => {
     it("should throw error when id is empty", () => {
         expect(() => {
             const product = new Product("", "Product 1", 100);
-        }).toThrow("Id is required")
+        }).toThrow("product: Id is required")
     });
 
     it("should throw error when name is empty", () => {
         expect(() => {
             const product = new Product("123", "", 100);
-        }).toThrow("Name is required")
+        }).toThrow("product: Name is required")
+    });
+
+    it("should throw error when id and name is empty", () => {
+        expect(() => {
+            const product = new Product("", "", 100);
+        }).toThrow("product: Id is required,product: Name is required")
     });
 
     it("should throw error when price is less than zero", () => {
